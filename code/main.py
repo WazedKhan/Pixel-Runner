a8566857444cd84b465d84b89138f245efbdf06d
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-18 17:40:33 +0600

    snail moving postions

--- code/main.py
+++ code/main.py
@@ -6,8 +6,16 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Pixel Runner')
 clock = pygame.time.Clock()
 
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+test_surface = test_font.render('Start', False, 'Black')
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+
+snail_x = 800
+snail_y = 280
+
 
 while True:
     for event in pygame.event.get():
@@ -15,8 +23,16 @@ while True:
             pygame.quit()
             sys.exit()
 
+
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(test_surface, (350, 50))
+
+    snail_x -= 2
+    if snail_x <= -72:
+        snail_x = 760
+
+    screen.blit(snail_surface, (snail_x, snail_y))
 
     pygame.display.update()
     clock.tick(60)
