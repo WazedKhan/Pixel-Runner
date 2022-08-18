69bbe4e82b4e701c2d1eaf9fa797019ddc7cd89b
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-18 16:58:31 +0600

    surface positions

--- code/main.py
+++ code/main.py
@@ -6,12 +6,16 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Pixel Runner')
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    screen.blit(test_surface, (200, 100))
+
     pygame.display.update()
     clock.tick(60)
-     
