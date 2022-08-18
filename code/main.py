d449f116d473b1162fd846fded65fef454e72fc2
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-18 17:17:38 +0600

    laoding image

--- code/main.py
+++ code/main.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Pixel Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +15,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.blit(test_surface, (200, 100))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
 
     pygame.display.update()
     clock.tick(60)
